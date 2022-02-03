f90de84e50e16e99a98d1f9f8781b0b5165c592b
Arif Chad <tech.arifchad@gmail.com>
2022-02-03 16:16:31 +0600

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+void main(){
+  runApp(WebViewExample());
+}
+class WebViewExample extends StatefulWidget {
+  @override
+  WebViewExampleState createState() => WebViewExampleState();
+}
+
+class WebViewExampleState extends State<WebViewExample> {
+  @override
+  void initState() {
+    super.initState();
+    // Enable virtual display.
+    if (Platform.isAndroid) WebView.platform = AndroidWebView();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return WebView(
+      initialUrl: 'https://www.bashtoilup.com/home',
+    );
+  }
+}
